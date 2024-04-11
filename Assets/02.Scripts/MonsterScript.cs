33e76b9c302a392d9727fb235b76161ec98354c9
이정한 <jason015634@gmail.com>
2024-04-11 15:58:39 +0900

    2024.04.11 jeonghan comiit

--- Assets/02.Scripts/MonsterScript.cs
+++ Assets/02.Scripts/MonsterScript.cs
@@ -4,29 +4,29 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.UIElements;
+using static Define;
 
 public class MonsterScript : MonoBehaviour
 {
-    public enum State { spawn, move, attack, dead };
+    MonsterState monsterState = MonsterState.spawn;
+   
+    public float monsterHP;
 
-    public State monsterState = State.spawn;
+    public float MonsterHP
+    { get { return monsterHP; } 
+      set { 
 
-    public int speed;
-
-    public int Speed
-    { 
-        get { return speed; }
-        set
-        {
-            speed = value;
-        } 
+            if(monsterState != MonsterState.spawn)
+            {
+                monsterHP = value;
+            }
+        }    
     }
 
-    public int monsterHP;
-    public int attack;
-    public int defense;
-    public int attackSpeed;
-    public int attackRange;
+    public float attack;
+    public float defense;
+    public float attackSpeed;
+    public float attackRange;
 
     public int moveSpeed;
     //private NavMeshAgent nvAgent;
@@ -73,29 +73,39 @@ public class MonsterScript : MonoBehaviour
         {
             switch (monsterState)
             {
-                case State.spawn:
+                case MonsterState.spawn:
                     UpdateSpawn();
                     break;
-                case State.move:
+                case MonsterState.move:
                     UpdateMove();
+                    UpdateBase();
                     break;
-                case State.attack:
+                case MonsterState.attack:
                     UpdateAttack();
+                    UpdateBase();
+                    break;
+                case MonsterState.dead:
+                    UpdateDead();
                     break;
             }
             yield return null;
         }
     }
 
-    bool IsAnimationPlaying(string animName)
+    void UpdateBase()
     {
-        return anim.GetCurrentAnimatorStateInfo(0).IsName(animName);
+        if (monsterHP == 0)
+        {
+            ChangeState(MonsterState.dead);
+            anim.SetBool("isDead", true);
+        }
+
     }
 
     void UpdateSpawn()
     {
         anim.SetTrigger("isSpawn");
-        ChangeState(State.move);
+        ChangeState(MonsterState.move);
     }
 
     void UpdateMove()
@@ -103,42 +113,51 @@ public class MonsterScript : MonoBehaviour
         //nvAgent.isStopped = false;
         //nvAgent.destination = target.position;
 
+        transform.position += LookAtPlayer() * moveSpeed * Time.deltaTime;
+
+        if (GetDistance(target.position, transform.position) < attackRange)
+        {
+            ChangeState(MonsterState.attack);
+            anim.SetBool("isAttack", true);
+        }
+    }
+
+    void UpdateAttack()
+    {
+        LookAtPlayer();
+
+        if (GetDistance(target.position, transform.position) > attackRange)
+        {
+            ChangeState(MonsterState.move);
+            anim.SetBool("isAttack", false);
+        }
+    }
+
+    void UpdateDead()
+    {
+        // 재화 및 경험치 얻는 처리?
+    }
+
+    void ChangeState(MonsterState state)
+    {
+        monsterState = state;
+    }
+
+    Vector3 LookAtPlayer()
+    {
         Vector3 followDirection = (target.position - transform.position).normalized;
 
         followDirection.y = 0f;
 
         transform.rotation = Quaternion.LookRotation(followDirection);
 
-       // float distance = (target.position - transform.position).magnitude;
-        transform.position += followDirection * moveSpeed * Time.deltaTime;
-
-        float dist = Vector3.Distance(target.position, transform.position);
-
-        if (dist < attackRange)
-        {
-            ChangeState(State.attack);
-            anim.SetBool("isAttack", true);
-        }
-
+        return followDirection;
     }
 
-    void UpdateAttack()
+    float GetDistance(Vector3 pos1, Vector3 pos2)
     {
-        float dist = Vector3.Distance(target.position, transform.position);
-
-        if (dist > attackRange)
-        {
-            ChangeState(State.move);
-            anim.SetBool("isAttack", false);
-        }
-
+        return Vector3.Distance(pos1, pos2);
     }
-
-    void ChangeState(State state)
-    {
-        monsterState = state;
-    }
-
 
     private void OnTriggerEnter(Collider other)
     {
